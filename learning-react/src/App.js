4bee7e29b78b430877abccbd63028706986c7440
Navaharish <navaharish2005@gmail.com>
2023-06-19 09:11:57 +0530

    Creating React app

---
+++ learning-react/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Greet from './components/welcome'
+
+function App() {
+  return (
+    <div className="App">
+
+      <p>hello everyone and how are you all</p>
+      <Greet> </Greet >
+
+    </div>
+  );
+}
+
+export default App;
---
+++ learning-react/src/components/welcome.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function greeting() {
+    return <h1>Hello Navaharish</h1>
+}
+
+export default greeting;
